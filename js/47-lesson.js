35b6cbfa6fa1f1dae9797848bae5c89ddf2b2cc1
Лида Рудова <102348531+Venyak@users.noreply.github.com>
2022-09-25 19:56:52 +0500

    solved lesson 47

---
+++ js/47-lesson.js
@@ -0,0 +1,10 @@
+/*
+	Реализуйте функцию с именем printMotto(), которая выведет на экран фразу Winter is coming.
+*/
+
+const printMotto = () => {
+  const text = 'Winter is coming';
+  console.log(text);
+};
+
+printMotto();
