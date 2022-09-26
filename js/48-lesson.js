f05e41ba796fc9890a3564dbab7c8f4391f19d22
Лида Рудова <102348531+Venyak@users.noreply.github.com>
2022-09-26 18:05:10 +0500

    solved lesson 48

---
+++ js/48-lesson.js
@@ -0,0 +1,9 @@
+/*
+	Реализуйте функцию sayHurrayThreeTimes(), которая возвращает строку 'hurray! hurray! hurray!'.
+*/
+
+const sayHurrayThreeTimes = () => {
+  return 'hurray! hurray! hurray!';
+};
+
+console.log(sayHurrayThreeTimes());
