63e940d0f7a571af164f7bede41daa16ecc5955b
Лида Рудова <102348531+Venyak@users.noreply.github.com>
2022-10-01 12:48:43 +0500

    Update 57-lesson.js

--- js/57-lesson.js
+++ js/57-lesson.js
@@ -2,8 +2,8 @@
 	Реализуйте функцию getLetter(), которая извлекает из переданной строки указанный символ (по порядковому номеру, а не по его индексу) и возвращает его наружу. Если такого символа нет, то функция возвращает пустую строку.
 */
 
-const getLetter = (word, number) => {
+const getLetter = (text, number) => {
   const index = number - 1;
-  const value = word[index] || '';
+  const value = text[index] || '';
   return value;
 };
