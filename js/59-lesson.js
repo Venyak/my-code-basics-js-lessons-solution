276122a5ddcd0f8b58bec224940d0bc975792a9d
Лида Рудова <102348531+Venyak@users.noreply.github.com>
2022-10-01 12:58:41 +0500

    Update 59-lesson.js

--- js/59-lesson.js
+++ js/59-lesson.js
@@ -2,10 +2,10 @@
 	Реализуйте функцию normalizeUrl(), которая выполняет так называемую нормализацию данных. Она принимает адрес сайта и возвращает его с https:// в начале.
 */
 
-const normalizeUrl = (adress) => {
-  if (adress.startsWith('https://')) {
+const normalizeUrl = (site) => {
+  if (site.startsWith('https://')) {
     return adress;
   } else {
-    return `https://${adress}`;
+    return `https://${site}`;
   }
 };
